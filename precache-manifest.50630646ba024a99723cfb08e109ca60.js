583547a6d669b2f2d2d41bd3ff4f95d19f442212
Tomas Fagerbekk <tomas@otovo.com>
2019-12-01 10:32:17 +0100

    Updates

---
+++ precache-manifest.50630646ba024a99723cfb08e109ca60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2919c08bc91504967d08bfc988833865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88702f761bb6d2f84b81",
+    "url": "/static/css/main.0d071fb3.chunk.css"
+  },
+  {
+    "revision": "5d1498f15ff387937c22",
+    "url": "/static/js/2.e4675c25.chunk.js"
+  },
+  {
+    "revision": "88702f761bb6d2f84b81",
+    "url": "/static/js/main.009c658f.chunk.js"
+  },
+  {
+    "revision": "c54c55a2e7ab78266d77",
+    "url": "/static/js/runtime-main.e9a72d07.js"
+  }
+]);
--- src/Components/styles.js
+++
@@ -1,4 +0,0 @@
-export const size = {
-  medium: `(min-width: 480px)`,
-  large: `(min-width: 768px)`,
-};
