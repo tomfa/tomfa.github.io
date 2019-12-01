09acd46cac7bcc930b523964d4e0cb578447dbcd
Tomas Fagerbekk <tomas@otovo.com>
2019-12-01 23:32:29 +0100

    Extract color palette as React hook util

--- src/Components/AwesomeSVG.js
+++ src/Components/AwesomeSVG.js
@@ -1,37 +1,22 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { usePalette } from '../utils/palette';
 
-const colorPalettes = [
-  ['#110133', '#00918e', '#4dd599', '#ffdc34'],
-  ['#ffa259', '#fe6845', '#fa4252', '#91bd3a'],
-  ['#561f55', '#8b2f97', '#cf56a1', '#fcb2bf'],
-  ['#dff6f0', '#46b3e6', '#4d80e4', '#2e279d'],
-  ['#f8b195', '#f67280', '#4d80e4', '#6c5b7b'],
-  ['#39375b', '#745c97', '#d597ce', '#f5b0cb'],
-  ['#9be3de', '#beebe9', '#fffdf9', '#ffe3ed'],
-  ['#eafbea', '#6f9a8d', '#1f6650', '#ea5e5e'],
-];
-
-const getRandomPalette = () => {
-  const palette =
-    colorPalettes[Math.floor(Math.random() * colorPalettes.length)];
-  return [...palette, ...palette, ...palette];
-};
-
-const getRandomSizeDistrubution = ({ width, height, palette }) => {
-  const numCircles = palette.length;
-  const maxRadius = Math.floor((2 * Math.min(width, height)) / numCircles);
+const getRandomSizeDistrubution = ({ width, height, count }) => {
+  const maxRadius = Math.floor((2 * Math.min(width, height)) / count);
   const minRadius = Math.floor(maxRadius / 8);
-  return palette.map(
-    () => minRadius + Math.floor(Math.random() * (maxRadius - minRadius))
-  );
+  return Array(count)
+    .fill()
+    .map(() => minRadius + Math.floor(Math.random() * (maxRadius - minRadius)));
 };
 
-const getRandomPositions = ({ width, height, radiuses }) => {
-  return radiuses.map(radius => [
-    Math.floor(Math.random() * width),
-    Math.floor(Math.random() * height),
-  ]);
+const getRandomPositions = ({ width, height, count }) => {
+  return Array(count)
+    .fill()
+    .map(radius => [
+      Math.floor(Math.random() * width),
+      Math.floor(Math.random() * height),
+    ]);
 };
 
 const SVGCircle = styled.circle`
@@ -44,12 +29,13 @@ const SVGCircle = styled.circle`
 `;
 
 export const AwesomeSVG = ({ width, height }) => {
-  const [palette, setPalette] = useState(getRandomPalette());
+  const [palette, setPalette] = usePalette();
+  const count = palette.length * 3;
   const [radiuses] = useState(
-    getRandomSizeDistrubution({ width, height, palette })
+    getRandomSizeDistrubution({ width, height, count })
   );
   const [positions, setPositions] = useState(
-    getRandomPositions({ width, height, radiuses })
+    getRandomPositions({ width, height, count })
   );
 
   return (
@@ -61,11 +47,11 @@ export const AwesomeSVG = ({ width, height }) => {
           r={radiuses[i]}
           stroke="#212121"
           strokeWidth="1"
-          fill={palette[i]}
+          fill={palette[i % palette.length]}
           onClick={() => {
-            setPositions(getRandomPositions({ width, height, radiuses }));
+            setPositions(getRandomPositions({ width, height, count }));
             if (Math.random() > 0.93) {
-              setPalette(getRandomPalette());
+              setPalette();
             }
           }}
         />
