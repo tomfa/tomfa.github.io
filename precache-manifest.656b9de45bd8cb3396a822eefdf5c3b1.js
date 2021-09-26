db631b5d7937b1a075a43f83650be0004f28bed4
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-09-26 23:14:40 +0200

    Updates

---
+++ precache-manifest.656b9de45bd8cb3396a822eefdf5c3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5e1405916f45d5c4e0979ea838a5b3",
+    "url": "https://tomfa.github.io/index.html"
+  },
+  {
+    "revision": "75a2b81768dee605544f",
+    "url": "https://tomfa.github.io/static/css/main.0c2e97f9.chunk.css"
+  },
+  {
+    "revision": "e954f86dfbd003d5f6bc",
+    "url": "https://tomfa.github.io/static/js/2.8c6f74ad.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "https://tomfa.github.io/static/js/2.8c6f74ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75a2b81768dee605544f",
+    "url": "https://tomfa.github.io/static/js/main.f6e6afde.chunk.js"
+  },
+  {
+    "revision": "214f1956b27345a217b2",
+    "url": "https://tomfa.github.io/static/js/runtime-main.a7bc6ae0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e21bc72eb38bcd2b880b3997166fe619.js"
+  "https://tomfa.github.io/precache-manifest.656b9de45bd8cb3396a822eefdf5c3b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://tomfa.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
